f136f5069b2270579b30da540dd0407252591b9e
zahid-official <zahid.official8@gmail.com>
2024-11-20 21:58:13 +0600

    Page Categories Update

--- react/src/Components/Learn/Category.jsx
+++ react/src/Components/Learn/Category.jsx
@@ -8,7 +8,7 @@ import cover from "/assets/cover.png";
 const Category = ({ category }) => {
     const {lesson_title, id} = category;
   return (
-    <Link to={`/lesson/${id}`}>
+    <Link to={`/learn/lesson/${id}`}>
       <div className="bg-[#eef5f7] text-[#186f7a] rounded-xl py-20 text-center px-5 hover:shadow-xl transition-all duration-300">
         <div className="flex justify-center relative">
           <img src={cover} alt="" />
