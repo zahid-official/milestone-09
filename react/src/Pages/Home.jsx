e986b5d6784d4150ede62bc6ce709f8e5cce8a84
zahid-official <zahid.official8@gmail.com>
2024-11-20 12:20:14 +0600

    Success Section

--- react/src/Pages/Home.jsx
+++ react/src/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import AboutSection from "../Components/AboutSection";
 import Banner from "../Components/Banner";
+import SuccessSection from "../Components/SuccessSection";
 import Title from "../Components/Title";
 
 const Home = () => {
@@ -7,9 +8,25 @@ const Home = () => {
     <div>
       <Banner></Banner>
 
-      <Title section={'About Elex'} heading1={'Upgrade your learning Skills'} heading2={'and Upgrade Life'}></Title>
-      <AboutSection></AboutSection>
+      {/* About Section */}
+      <section>
+        <Title
+          section={"About Elex"}
+          heading1={"Upgrade your learning Skills"}
+          heading2={"and Upgrade Life"}
+        ></Title>
+        <AboutSection></AboutSection>
+      </section>
 
+      {/* Success Section */}
+      <section className="bg-[#eef5f7] mt-10">
+        <Title
+          section={"Start To Success"}
+          heading1={"Start your journey & Achieve your goal"}
+          heading2={""}
+        ></Title>
+        <SuccessSection></SuccessSection>
+      </section>
     </div>
   );
 };
